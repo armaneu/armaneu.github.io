2785c83eb3cb7e6b3e9c22a92cf8f8e8b6647a9b
Armando Abreu Duque <abreu.dev.github@gmail.com>
2024-01-21 14:05:30 -0500

    testing #2 of the navbar menu

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,17 @@
-function OpenCloseBtn(x) {
-    x.classList.toggle("change");
+function ShowHideMenu() {
+  var element = document.getElementById("main_topnav");
+
+  if (element.className === "topnav") {
+    element.className += " responsive";
+  } else {
+    element.className = "topnav";
   }
+}
+
+function OpenCloseBtn(div) {
+  
+  div.classList.toggle("change");
+
+  ShowHideMenu();
+}
+
