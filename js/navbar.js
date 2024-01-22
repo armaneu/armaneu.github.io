81d9500ba7c4ff930d81b492ecc6778e76fdf9bc
Armando Abreu Duque <abreu.dev.github@gmail.com>
2024-01-21 22:06:18 -0500

    testing #3 of the navbar menu

--- js/navbar.js
+++ js/navbar.js
@@ -2,11 +2,11 @@ function CloseMenuOnClickMain() {
   var element = document.getElementById("main_topnav");
 
   if (element.className !== "topnav") {
-    element.className = "topnav"; 
-  }
+    element.className = "topnav";
 
-  var div = document.getElementById("id_open_close_btn");
-  div.classList.toggle("change");
+    var div = document.getElementById("id_open_close_btn");
+    div.classList.toggle("change");
+  }
 }
 
 function CloseMenu() {
