cff573383a56fd40b9ce284db37cec24ecea36a4
Armando Abreu Duque <abreu.dev.github@gmail.com>
2024-01-21 22:00:08 -0500

    adding some Javascript functionalities to the navbar menu

--- js/navbar.js
+++ js/navbar.js
@@ -1,3 +1,22 @@
+function CloseMenuOnClickMain() {
+  var element = document.getElementById("main_topnav");
+
+  if (element.className !== "topnav") {
+    element.className = "topnav"; 
+  }
+
+  var div = document.getElementById("id_open_close_btn");
+  div.classList.toggle("change");
+}
+
+function CloseMenu() {
+  var element = document.getElementById("main_topnav");
+  element.className = "topnav";
+
+  var div = document.getElementById("id_open_close_btn");
+  div.classList.toggle("change");
+}
+
 function ShowHideMenu() {
   var element = document.getElementById("main_topnav");
 
